3be81641e6db93205358ce501814fa6ba752ddc1
joaovitorsantanasdsjohn-crypto <joaovitorsantanasdsjohn@gmail.com>
2025-10-18 16:01:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -297,10 +297,11 @@ def home():
 def start():
     t = threading.Thread(target=loop_bot, daemon=True)
     t.start()
-    app.run(host="0.0.0.0", port=PORT)
+    app.run(host="0.0.0.0", port=8080)
 
 if __name__ == "__main__":
     logger.info("Inicializando Indicador Trading S1000...")
     start()
+
 
 
