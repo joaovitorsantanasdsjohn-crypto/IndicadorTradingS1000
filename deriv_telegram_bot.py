25734c111ec2044073295b418c6769e6e7bf0300
joaovitorsantanasdsjohn-crypto <joaovitorsantanasdsjohn@gmail.com>
2026-02-23 11:31:06 -0300

    Update deriv_telegram_bot.py

--- deriv_telegram_bot.py
+++ deriv_telegram_bot.py
@@ -424,8 +424,7 @@ async def ws_loop(symbol):
 
                     # 🔓 auto reset proposal lock travado
                     if proposal_lock[symbol]:
-                        if time.time() - 
-                        proposal_lock_time.get(symbol, 0) > 60:
+                        if time.time() - proposal_lock_time.get(symbol, 0) > 60:
                              proposal_lock[symbol] = False
                              log(f"{symbol} proposal lock reset automático")
 
