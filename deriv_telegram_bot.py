9278f1c3eb77c43e58cd5821e2bbbd2d471f9a0b
joaovitorsantanasdsjohn-crypto <joaovitorsantanasdsjohn@gmail.com>
2026-03-09 23:15:14 -0300

    Add ML_DATA_FILE constant for machine learning data

--- deriv_telegram_bot.py
+++ deriv_telegram_bot.py
@@ -84,6 +84,7 @@ REQ_ID_SEQ=1
 daily_pnl=0
 trading_paused=False
 STATE_FILE = "daily_state.json"
+ML_DATA_FILE = "ml_data.json"
 current_day = datetime.now(timezone.utc).date()
 
 #=============================================================
