c3ecf93b15dae88b756516557cd18df2d7330798
joaovitorsantanasdsjohn-crypto <joaovitorsantanasdsjohn@gmail.com>
2026-03-30 14:19:14 -0300

    Update deriv_telegram_bot.py

--- deriv_telegram_bot.py
+++ deriv_telegram_bot.py
@@ -853,7 +853,7 @@ async def ws_loop(symbol):
                         
                         confidence_gap = abs(prob_up - prob_down)
 
-                        if confidence_gap < 0.08:
+                        if confidence_gap < 0.05:
                             continue
 
                         # ===================================================
